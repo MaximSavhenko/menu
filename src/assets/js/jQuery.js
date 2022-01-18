d2eebb652d7af005510ca99aa49a4f7f5fc6f946
Maxim Savchenko <maxim.savchenko@wdg.com.ua>
2022-01-18 19:40:03 +0200

    make jQuery code (burger and subMenu)

---
+++ src/assets/js/jQuery.js
@@ -0,0 +1,62 @@
+jQuery(function () {
+  function burgerToggle() {
+    let btnMenu = $(".header__btn-menu"),
+      burger = $(".mobile-menu");
+
+    btnMenu.on('click' , function () {
+        btnMenu.addClass("open");
+        burger.addClass("open");
+    })
+  }
+
+  burgerToggle();
+  function menuSublinkAddClass() {
+    let menu = $(".mobile-menu .mobile-menu__link"),
+      menu_mobile = $("#mobile-menu__id");
+
+    menu.each(function () {
+      if (!$(this).children(".mobile-menu__submenu").length > 0) {
+        $(this).addClass("empty");
+      }
+    });
+
+    menu_mobile.on("click", function (event) {
+      let target = $(event.target),
+        then = target.hasClass("mobile-menu__link");
+
+      function closeMenu() {
+        let subm = $(".mobile-menu__submenu");
+        if (menu_mobile) {
+          subm.hide();
+        }
+      }
+
+      if (then) {
+        let subMenu = target.find(".mobile-menu__submenu");
+        closeMenu();
+        subMenu.each(function () {
+          $(this).fadeIn(50);
+        });
+      }
+
+      $(document).on("click", function (e) {
+        let subm = $(".mobile-menu__submenu"),
+          burger = $(".mobile-menu"),
+          btnMenu = $(".header__btn-menu");
+        if (
+          !menu.is(e.target) &&
+          menu_mobile.has(e.target).length === 0 &&
+          !subm.is(e.target) &&
+          subm.has(e.target).length === 0 &&
+          !btnMenu.is(e.target) &&
+          btnMenu.has(e.target).length === 0
+        ) {
+          burger.removeClass("open");
+          closeMenu();
+        }
+      });
+    });
+  }
+
+  menuSublinkAddClass();
+});
